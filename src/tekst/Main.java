48143ef8118a43e6511784e697a2e59a88de9fbf
bruger <bruger@10.200.123.110>
2016-10-31 10:25:01 +0100

    ændring 1

--- src/tekst/Main.java
+++ src/tekst/Main.java
@@ -17,6 +17,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        System.out.println("hej med jer");    }
+        System.out.println("hej med jer gutter");    }
     
 }
